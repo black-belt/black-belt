93cb920d402d8d1ba8a8787f8628bcd9118a2cc3
rosieyeon <sseyeon956@gmail.com>
2022-03-10 16:59:00 +0900

    ✨ wip: font, colors 설정

---
+++ client/src/_foundation/colors/colors.js
@@ -0,0 +1,25 @@
+const colors = {
+  blue0: "#00B7BE",
+  blue1: "#004C8D",
+  blue2: "#003C74",
+
+  star: "#E5B560",
+
+  white: "#FFFFFF",
+  black: "#292D32",
+
+  gray0: "#F8F9FA",
+  gray1: "#F1F3F5",
+  gray2: "#E9ECEF",
+  gray3: "#DEE2E6",
+  gray4: "#CED4DA",
+  gray5: "#ADB5BD",
+  gray6: "#868E96",
+  gray7: "#4A5056",
+  gray8: "#3D4248",
+  gray9: "#31363B",
+
+  error: "#941E34",
+};
+
+export default colors;
---
+++ client/src/_foundation/typography/fontWeight.js
@@ -0,0 +1,8 @@
+const fontWeight = {
+  regular: "400",
+  medium: "500",
+  bold: "700",
+  extrabold: "800",
+};
+
+export default fontWeight;
