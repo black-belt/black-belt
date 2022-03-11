13ae2be8e9cb5f3b43eea0ed927be3e460cd3666
rosieyeon <sseyeon956@gmail.com>
2022-03-11 14:36:46 +0900

    🚀 feat: add lang btn

---
+++ client/src/components/atoms/Buttons/lang-btn.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { colors, fontSize, fontWeight } from "_foundation";
+import Icon from "../Icons/Icon";
+
+function LangBtn({ children }) {
+  return <StyledBtn>{children}</StyledBtn>;
+}
+export default LangBtn;
+
+const StyledBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${(props) => props.width};
+  height: 36px;
+  padding: 1rem 1rem 1rem 0.6rem;
+
+  border-radius: 4rem;
+  border: 1px solid ${colors.gray0};
+  color: ${colors.gray0};
+  background-color: transparent;
+
+  font-size: ${fontSize.lg};
+  line-height: 1.75rem;
+  text-align: center;
+  text-decoration: none;
+
+  transition: 0.2s;
+  cursor: pointer;
+
+  ${(props) =>
+    css`
+      :hover {
+        background: ${colors.blue1};
+      }
+    `}
+
+  svg {
+    width: 25px;
+    height: 25px;
+    margin-right: 10px;
+    vertical-align: top;
+  }
+`;
