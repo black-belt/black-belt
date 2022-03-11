8cc1e4d30a8af41c9b1b1416615ec29402c0a52b
rosieyeon <sseyeon956@gmail.com>
2022-03-11 13:15:56 +0900

    ✨ wip: button component

--- client/src/components/atoms/Buttons/in-btns.js
+++ client/src/components/atoms/Buttons/in-btns.js
@@ -1,1 +1,45 @@
-<div>hihi</div>;
+import React from "react";
+import styled, { css } from "styled-components";
+import { colors, fontSize, fontWeight } from "_foundation";
+
+function InButton({ children }) {
+  return <StyledBtn>{children}</StyledBtn>;
+}
+export default InButton;
+
+const StyledBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: ${(props) => props.width};
+  height: 48px;
+  padding: 0.6rem 1.75rem;
+
+  border-radius: 4rem;
+  border: 1px solid ${colors.gray0};
+  color: ${colors.gray0};
+  background-color: transparent;
+
+  font-size: ${fontSize.xl};
+  line-height: 1.75rem;
+  text-align: center;
+  text-decoration: none;
+
+  transition: 0.2s;
+  cursor: pointer;
+
+  ${(props) =>
+    css`
+      :hover {
+        background: ${colors.blue1};
+      }
+    `}
+
+  svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    vertical-align: top;
+  }
+`;
--- client/src/pages/Practice/Combos.js
+++ client/src/pages/Practice/Combos.js
@@ -1,5 +1,12 @@
+import InButton from "../../components/atoms/Buttons/in-btns";
+
 function Combos() {
-  return <div className="Combos">Combos</div>;
+  return (
+    <div className="Combos">
+      Combos
+      <InButton>Button test</InButton>
+    </div>
+  );
 }
 
 export default Combos;
