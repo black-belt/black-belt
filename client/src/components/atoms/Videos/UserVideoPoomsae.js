36423ac9f2a0f8ac0ccedd4f198c25365f31ab22
chung1997jy <chung1997jy@gmail.com>
2022-03-14 17:25:44 +0900

    [#S06P22A506-113] :sparkles:wip: poomsae stage

---
+++ client/src/components/atoms/Videos/UserVideoPoomsae.js
@@ -0,0 +1,108 @@
+import { useRef, useEffect, useState } from "react";
+import styled from "styled-components";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from "@tensorflow/tfjs";
+
+function UserVideoPoomsae({
+  answer,
+  updateIsPass,
+  testResult,
+  setNextAction,
+  isPass,
+  setPartIndex,
+  // partIndex,
+}) {
+  const videoRef = useRef(null);
+  let net;
+  let nextAction = 0;
+  let partIndex = 0;
+  let testAvg = 0.0;
+  let curActionIdx = 0;
+  const [webCamElement, setWebCamElement] = useState();
+  const getWebcam = (callback) => {
+    try {
+      const constraints = {
+        video: true,
+        audio: false,
+      };
+      navigator.mediaDevices.getUserMedia(constraints).then(callback);
+    } catch (err) {
+      console.log(err);
+      return undefined;
+    }
+  };
+
+  const run = async () => {
+    net = await mobilenet.load();
+    const webcam = await tf.data.webcam(webCamElement, {
+      resizeWidth: 220,
+      resizeHeight: 227,
+    });
+    while (answer !== "" && answer.length > 0 && nextAction < answer.length) {
+      const img = await webcam.capture();
+      const result = await net.classify(img);
+      console.log(partIndex, nextAction, answer, result[0].className, result[0].probability);
+      img.dispose();
+      curActionIdx = answer.indexOf(result[0].className.split(",")[0]);
+      if (curActionIdx >= 0) {
+        if (answer[nextAction] === result[0].className.split(",")[0]) {
+          testAvg += result[0].probability;
+          nextAction++;
+          if (nextAction === answer.length) {
+            //현단락에서 마지막동작
+            setNextAction(0);
+            nextAction = 0;
+            if (partIndex === 3) {
+              //마지막 단락
+              setPartIndex(0);
+              partIndex = 0;
+              testResult(testAvg);
+              updateIsPass();
+              const s = videoRef.current.srcObject;
+              s.getTracks().forEach((track) => {
+                track.stop();
+              });
+              break;
+            } else {
+              partIndex = partIndex + 1;
+              setPartIndex((current) => current + 1);
+            }
+          } else {
+            setNextAction((current) => current + 1);
+          }
+        } else {
+          curActionIdx = -1;
+        }
+      }
+      await tf.nextFrame();
+    }
+  };
+
+  useEffect(() => {
+    if (!isPass) {
+      getWebcam((stream) => {
+        videoRef.current.srcObject = stream;
+        setWebCamElement(videoRef.current);
+        // run();
+      });
+    }
+  }, [isPass]);
+
+  useEffect(() => {
+    run();
+  }, [answer, isPass]);
+
+  return (
+    <>
+      <VideoContainer ref={videoRef} autoPlay />
+    </>
+  );
+}
+
+export default UserVideoPoomsae;
+
+const VideoContainer = styled.video`
+  height: 22vw;
+  margin-bottom: 60px;
+  border-radius: 10px;
+`;
--- client/src/pages/Practice/PoomsaeStage.js
+++ client/src/pages/Practice/PoomsaeStage.js
@@ -1,5 +1,178 @@
+import PracticeStageTemplate from "../../components/templates/PracticeStageTemplate";
+import { useEffect, useState } from "react";
+import LocalVideo from "../../components/atoms/Videos/LocalVideo";
+import { useNavigate } from "react-router-dom";
+import EvaluationTemplate from "components/templates/EvaluationTemplate";
+import DescStage from "components/atoms/Texts/DescStage";
+import StageBtn from "components/atoms/Buttons/stage-btn";
+import UserVideoPoomsae from "components/atoms/Videos/UserVideoPoomsae";
+import PartStage from "components/atoms/Texts/PartStage";
+
 function PoomsaeStage() {
-  return <div className="PoomsaeStage">PoomsaeStage</div>;
+  const [videoSelected, setVideoSelected] = useState("../../videos/basics1.MP4");
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState([[]]);
+  const [answerOrigin, setAnswerOrigin] = useState([[]]);
+  const [answer, setAnswer] = useState([[]]);
+  const [answerIndex, setAnswerIndex] = useState([[]]);
+  const [isPass, setIsPass] = useState(false);
+  const [grade, setGrade] = useState("Try Again");
+  const [gradeNum, setGradeNum] = useState(0);
+  const [nextAction, setNextAction] = useState(0);
+  const [part, setPart] = useState(["1단락", "2단락", "3단락", "4단락"]); //영어버전
+  const [partIndex, setPartIndex] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //받기: api 통신해서 title, description[[]], 비디오(Streaming이나 youtube link), 답안[[]], 답안의 인덱스[[]] 받음
+    //보내기: 서버로 통과 단계를 보냄
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((json) => {
+        setTitle("품새");
+        setDesc([
+          [
+            "왼 아래(내려)막기",
+            "오른 (몸통)지르기",
+            "오른 아래(내려)막기",
+            "왼 (몸통)지르기",
+            "왼 아래(내려)막기",
+            "오른 (몸통)지르기",
+          ],
+          [
+            "2왼 아래(내려)막기",
+            "2오른 (몸통)지르기",
+            "2오른 아래(내려)막기",
+            "2왼 (몸통)지르기",
+            "2왼 아래(내려)막기",
+            "2오른 (몸통)지르기",
+          ],
+          [
+            "3왼 아래(내려)막기",
+            "3오른 (몸통)지르기",
+            "3오른 아래(내려)막기",
+            "3왼 (몸통)지르기",
+            "3왼 아래(내려)막기",
+            "3오른 (몸통)지르기",
+          ],
+          [
+            "4왼 아래(내려)막기",
+            "4오른 (몸통)지르기",
+            "4오른 아래(내려)막기",
+            "4왼 (몸통)지르기",
+            "4왼 아래(내려)막기",
+            "4오른 (몸통)지르기",
+          ],
+        ]);
+        setAnswerOrigin([
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+        ]);
+        setAnswer([
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+          ["coffee mug", "coffee mug", "coffee mug"],
+        ]);
+        setAnswerIndex([
+          [0, 2, 4, 5],
+          [0, 2, 4, 5],
+          [0, 2, 4, 5],
+          [0, 2, 4, 5],
+        ]);
+        setVideoSelected(`https://youtu.be/o9JvP-A4TvY`);
+      });
+
+    return () => {
+      fetch("https://jsonplaceholder.typicode.com/posts");
+    };
+  }, []);
+
+  const updateIsPass = () => {
+    setNextAction(0);
+    setPartIndex(0);
+    // setAnswer([[]]);
+    setIsPass(true);
+  };
+
+  const testResult = (result) => {
+    let num = 0;
+    answer.forEach((ans) => {
+      num += ans.length;
+    });
+    result /= num;
+    if (result >= 0.8) {
+      setGrade("Perfect!");
+      setGradeNum(3);
+    } else if (result >= 0.7) {
+      setGrade("Great");
+      setGradeNum(2);
+    } else if (result >= 0.6) {
+      setGrade("Good");
+      setGradeNum(1);
+    }
+  };
+
+  // const updatePartIndex = (props) => {
+  //   if (props === 1) setPartIndex((current) => current + 1);
+  //   else setPartIndex(0);
+  // };
+
+  const restartFunc = () => {
+    setIsPass(false);
+    setAnswer(answerOrigin);
+  };
+  const homeFunc = () => {
+    navigate("/");
+  };
+
+  // console.log(
+  //   "part",
+  //   partIndex,
+  //   "nextAc",
+  //   nextAction,
+  //   "answerIndex",
+  //   answerIndex
+  //   // "value",
+  //   // answerIndex[partIndex][nextAction]
+  // );
+
+  return (
+    <>
+      <PracticeStageTemplate
+        title={title}
+        partStage={<PartStage partArray={part} curIdx={partIndex} />}
+        desc={<DescStage descArray={desc[partIndex]} curIdx={answerIndex[partIndex][nextAction]} />}
+        video={<LocalVideo url={videoSelected} />}
+        camera={
+          <UserVideoPoomsae
+            answer={answer[partIndex]}
+            updateIsPass={updateIsPass}
+            testResult={testResult}
+            setNextAction={setNextAction}
+            setPartIndex={setPartIndex}
+            partIndex={partIndex}
+            isPass={isPass}
+          />
+        }
+      />
+
+      {isPass ? (
+        <EvaluationTemplate
+          grade={grade}
+          gradeNum={gradeNum}
+          restart={<StageBtn onClick={restartFunc}>다시하기</StageBtn>}
+          home={
+            <StageBtn onClick={homeFunc} isHome>
+              홈으로 이동
+            </StageBtn>
+          }
+        />
+      ) : null}
+    </>
+  );
 }
 
 export default PoomsaeStage;
