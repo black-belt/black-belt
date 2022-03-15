9735c8d119512aeb6412536ef9c048c9e8a88075
rosieyeon <sseyeon956@gmail.com>
2022-03-15 20:11:29 +0900

    🚀 feat: add dry-brush font

--- client/src/pages/Practice/Combos.js
+++ client/src/pages/Practice/Combos.js
@@ -1,5 +1,12 @@
+import LoginModal from "components/organisms/LoginModal/LoginModal";
+
 function Combos() {
-  return <div className="Combos">Combos</div>;
+  return (
+    <div className="Combos">
+      Combos
+      <LoginModal />
+    </div>
+  );
 }
 
 export default Combos;
