7c72c05961e110c353ba138d091625bd67d23501
rosieyeon <sseyeon956@gmail.com>
2022-03-19 12:01:44 +0900

    [#22]✨ wip: mainpage wireframe

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -4,7 +4,6 @@ function MainPage() {
   return (
     <div className="MainPage">
       <Background>
-        {/* <BackgroundVideo url="videos/background.mp4"></BackgroundVideo> */}
         <BackgroundVideo url="videos/background.mp4"></BackgroundVideo>
       </Background>
       <Layer></Layer>
