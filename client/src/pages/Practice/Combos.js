c4e2f20a59be0e2b560fff1212977578c3507700
rosieyeon <sseyeon956@gmail.com>
2022-03-23 13:43:17 +0900

    ✨ wip: merge conflict

--- client/src/pages/Practice/Combos.js
+++ client/src/pages/Practice/Combos.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-import LoginModal from "components/organisms/LoginModal/LoginModal";
-=======
-import Navbar from "components/molecules/Navbar";
->>>>>>> abd007485d6ee306a4e2d6d4f9c3ef4be7d6a6ed
-
 function Combos() {
-  return (
-    <div className="Combos">
-      Combos
-<<<<<<< HEAD
-      <LoginModal />
-=======
-      <Navbar />
->>>>>>> abd007485d6ee306a4e2d6d4f9c3ef4be7d6a6ed
-    </div>
-  );
+  return <div className="Combos">Combos</div>;
 }
 
 export default Combos;
