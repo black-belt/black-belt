621121ab1c87be79af2a9a792a3fa8541b21bc69
chung1997jy <chung1997jy@gmail.com>
2022-03-25 02:20:32 +0900

    :fire:hotfix: delete videos

---
+++ client/src/components/templates/BasicTemplate.js
@@ -0,0 +1,18 @@
+// import styled from "styled-components";
+
+// function BasicTempalte() {
+//   return (
+//     <Container>
+//       <TextContainer></TextContainer>
+//       <CardContainer></CardContainer>
+//     </Container>
+//   );
+// }
+
+// export default BasicTempalte;
+
+// const Container = styled.div``;
+
+// const TextContainer = styled.div``;
+
+// const CardContainer = styled.did``;
