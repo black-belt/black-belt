ca12bbe0e604ba1ff8ee0ff89f0bec4b0f3ca333
rosieyeon <sseyeon956@gmail.com>
2022-03-25 02:31:02 +0900

    ✨ wip: add api.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL,
+  timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("blackbelt_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use((config) => {
+  return config.data;
+});
+export default axiosInstance;
