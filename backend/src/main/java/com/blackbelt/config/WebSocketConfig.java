20fda2fadc86413224ef65814630b7f35f557b2b
yunsoo-choi <67628725+yunsoo-choi@users.noreply.github.com>
2022-03-28 10:53:26 +0900

    Update WebSocketConfig.java
    간단한 수정

--- backend/src/main/java/com/blackbelt/config/WebSocketConfig.java
+++ backend/src/main/java/com/blackbelt/config/WebSocketConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
+/*
 //웹소켓 관련 설정 파일
 @Configuration
 @EnableWebSocketMessageBroker
@@ -18,3 +18,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 		registry.addEndpoint("/").setAllowedOrigins("*").withSockJS();
 	}
 }
+*/
