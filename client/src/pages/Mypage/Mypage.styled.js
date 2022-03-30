62030270cbc6e9e1b545231c9a2be73e53a00928
rosieyeon <sseyeon956@gmail.com>
2022-03-30 12:56:51 +0900

    ✨ wip: add styled.js

---
+++ client/src/pages/Mypage/Mypage.styled.js
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+export const Layout = styled.div``;
+
+export const ProfileBox = styled.div``;
+
+export const ProfileImg = styled.img``;
+
+export const Username = styled.div``;
+
+export const UserEmail = styled.div``;
