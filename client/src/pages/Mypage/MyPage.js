d2d8f84124845ce83e1ea542e60f2647a1aba5f6
rosieyeon <sseyeon956@gmail.com>
2022-04-02 01:33:19 +0900

    ✨ wip: add mypage layout

--- client/src/pages/Mypage/MyPage.js
+++ client/src/pages/Mypage/MyPage.js
@@ -1,5 +1,38 @@
+import {
+  Carousel,
+  GyeorugiInfo,
+  ImgBox,
+  ImgWrapper,
+  Layout,
+  MyInfo,
+  ProfileBox,
+  ProfileImg,
+  TierImg,
+  UserCountry,
+  UserEmail,
+  Username,
+} from "./Mypage.styled";
+
 function MyPage() {
-  return <div className="MyPage">MyPageeeee</div>;
+  return (
+    <div className="MyPage">
+      <Layout>
+        <ProfileBox>
+          <ImgBox>
+            <ImgWrapper>{/* <ProfileImg /> */}</ImgWrapper>
+          </ImgBox>
+          <UserCountry></UserCountry>
+          <Username></Username>
+          <UserEmail></UserEmail>
+          {/* <TierImg/> */}
+        </ProfileBox>
+        <MyInfo>
+          <Carousel></Carousel>
+          <GyeorugiInfo></GyeorugiInfo>
+        </MyInfo>
+      </Layout>
+    </div>
+  );
 }
 
 export default MyPage;
--- client/src/pages/Mypage/Mypage.styled.js
+++ client/src/pages/Mypage/Mypage.styled.js
@@ -4,10 +4,22 @@ export const Layout = styled.div``;
 
 export const ProfileBox = styled.div``;
 
+export const ImgBox = styled.div``;
+
+export const ImgWrapper = styled.div``;
+
 export const ProfileImg = styled.img``;
+
+export const UserCountry = styled.div``;
 
 export const Username = styled.div``;
 
 export const UserEmail = styled.div``;
 
-export const Test = styled.div``;
+export const TierImg = styled.img``;
+
+export const MyInfo = styled.div``;
+
+export const Carousel = styled.div``;
+
+export const GyeorugiInfo = styled.div``;
