30b7bdf2b69fda36c78a5477d4a9b27e263bfeb9
rosieyeon <sseyeon956@gmail.com>
2022-04-04 12:25:28 +0900

    ✨ wip: connect websocket

--- client/src/pages/MainPage/startWS.js
+++ client/src/pages/MainPage/startWS.js
@@ -4,14 +4,9 @@ import StompJs from "stompjs";
 const sock = new SockJs("https://j6a506.p.ssafy.io/stomp/");
 const stomp = StompJs.over(sock);
 
-let Msg = [];
-const recvMsg = () => {};
-
 export const StartWS = (userId) => {
-  console.log("here?");
   try {
     stomp.connect({}, () => {
-      console.log("connected");
       stomp.subscribe(`/sub/api/que/user/${userId}`, (data) => {
         const newMessage = JSON.parse(data.body);
         console.log(newMessage);
@@ -30,6 +25,5 @@ export const Invite = (props) => {
     guestId: Number(props.guestId),
     token: props.token,
   };
-  // stomp.send(`/pub/api/que/user/${props.guestId}`, {}, JSON.stringify(data));
   stomp.send("/pub/api/que/user", {}, JSON.stringify(data));
 };
