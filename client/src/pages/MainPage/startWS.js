e84a9bbb23c070cbc496e16a170c7d154a28643e
rosieyeon <sseyeon956@gmail.com>
2022-04-06 13:05:30 +0900

    ✨ wip: change server url

--- client/src/pages/MainPage/startWS.js
+++ client/src/pages/MainPage/startWS.js
@@ -2,7 +2,7 @@ import SockJs from "sockjs-client";
 import StompJs from "stompjs";
 // import { useNavigate } from "react-router-dom";
 
-const sock = new SockJs("http://localhost:8000/ws-stomp/");
+const sock = new SockJs("https://j6a506.p.ssafy.io/stomp/");
 const stomp = StompJs.over(sock);
 
 export const StartWS = (userId) => {
