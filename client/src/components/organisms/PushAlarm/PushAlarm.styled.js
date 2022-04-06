ac85c0822c27b98bde1dec4ef79d38d398fb2b66
rosieyeon <sseyeon956@gmail.com>
2022-04-06 10:56:34 +0900

    ✨ wip: change push alarm background color

--- client/src/components/organisms/PushAlarm/PushAlarm.styled.js
+++ client/src/components/organisms/PushAlarm/PushAlarm.styled.js
@@ -16,9 +16,9 @@ export const OverLay = styled.div`
 
 export const ModalBox = styled.div`
   position: fixed;
-  top: 150px;
+  /* top: 150px; */
   right: 15px;
-  /* bottom: 0; */
+  bottom: 0;
   /* left: 0; */
   z-index: 10000;
 
@@ -34,13 +34,13 @@ export const ModalSection = styled.div`
   top: 50%;
 
   width: 350px;
-  height: 120px;
+  height: 140px;
   /* margin: auto; */
   padding: 1rem;
   border-radius: 10px;
 
   /* background-color: rgba(222, 226, 230, 0.2); */
-  background-color: rgba(0, 0, 0, 0.45);
+  background-color: rgba(73, 80, 87, 0.45);
   backdrop-filter: blur(10px);
   transform: translateY(-50%);
   animation: 0.4s cubic-bezier(0.3, 0, 0, 1);
@@ -67,15 +67,19 @@ export const ModalHeader = styled.div`
 `;
 
 export const ModalContent = styled.div`
-  background-color: red;
+  /* background-color: red; */
   height: 68%;
+  padding: 0.5rem 0;
 
   color: ${colors.gray1};
 `;
 
 export const ButtonBox = styled.div`
   display: flex;
+  justify-content: flex-end;
   gap: 1rem;
+
+  padding: 0.5rem;
 `;
 
 // export const
