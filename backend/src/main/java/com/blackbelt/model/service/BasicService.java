fabdfb0379f6726fb79562ee170637546c6e153c
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-04-07 03:23:17 +0900

    [#S06P22A506-161] 🔥 hotfix: basic API fix

--- backend/src/main/java/com/blackbelt/model/service/BasicService.java
+++ backend/src/main/java/com/blackbelt/model/service/BasicService.java
@@ -10,5 +10,6 @@ public interface BasicService {
 	List<BasicDto> listBasic(String user_id) throws Exception;
 	BasicDto getBasic(String user_id, int basicid) throws Exception;
 	void updateBasicStage(BasicStageDto basicStageDto) throws Exception;
+	String nowlevel(String user_id) throws Exception;;
 	
 }
--- backend/src/main/java/com/blackbelt/model/service/BasicServiceImpl.java
+++ backend/src/main/java/com/blackbelt/model/service/BasicServiceImpl.java
@@ -25,15 +25,33 @@ public class BasicServiceImpl implements BasicService {
 	}
 	@Override
 	public void updateBasicStage(BasicStageDto basicStageDto) throws Exception {
+		int user_id = Integer.parseInt(basicStageDto.getUser_id());
+		int basic_id = Integer.parseInt(basicStageDto.getBasic_id());
+		BasicDto basicData = sqlSession.getMapper(BasicMapper.class).getBasic(user_id, basic_id);
+		int pre_score = 0;//null이면 0이 들어감.
+		if(basicData.getBasic_score() != null) {
+			pre_score = Integer.parseInt(basicData.getBasic_score());//현재 값
+		}
+		int after_score = Integer.parseInt(basicStageDto.getBasic_score());//바꿀 값..
+
+		if(pre_score < after_score) {//바꿀값이 더 크면
+			sqlSession.getMapper(BasicMapper.class).levelup(basicStageDto.getUser_id());
+			sqlSession.getMapper(BasicMapper.class).updateBasicStage(basicStageDto);
+		}
 		
-		sqlSession.getMapper(BasicMapper.class).levelup(basicStageDto.getUser_id());
-		sqlSession.getMapper(BasicMapper.class).updateBasicStage(basicStageDto);
 		
 	}
 	@Override
 	public List<BasicDto> listBasic(String user_id) throws Exception {
 		return sqlSession.getMapper(BasicMapper.class).listBasicuser(user_id);
 	}
+	@Override
+	public String nowlevel(String user_id_str) throws Exception {
+		// TODO Auto-generated method stub
+		int user_id = Integer.parseInt(user_id_str); 
+		return sqlSession.getMapper(BasicMapper.class).nowlevel(user_id);
+	}
+	
 	
 
 
