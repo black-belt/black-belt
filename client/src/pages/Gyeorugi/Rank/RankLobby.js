e93acc1f20b554720dc6e520ec6a2a84ade7fae1
rosieyeon <sseyeon956@gmail.com>
2022-04-07 11:27:08 +0900

    ✨ wip: design rank lobby

--- client/src/pages/Gyeorugi/Rank/RankLobby.js
+++ client/src/pages/Gyeorugi/Rank/RankLobby.js
@@ -1,8 +1,58 @@
+import InButton from "components/atoms/Buttons/in-btns";
+import { useTranslation } from "react-i18next";
+import { useRecoilValue } from "recoil";
+import { userInfo } from "recoils";
+import {
+  Background,
+  Champion,
+  ChampionBox,
+  ChampionInfo,
+  ImgWrapper,
+  Layout,
+  Name,
+  ProfileImg,
+  ProfileImgBox,
+  Tier,
+} from "./RankLobby.styled";
+
 function RankLobby() {
+  const { t } = useTranslation();
+  const myInfo = useRecoilValue(userInfo);
+  const tier = {
+    1: "bronze",
+    2: "silver",
+    3: "gold",
+    4: "platinum",
+    5: "diamond",
+    6: "master",
+  };
+  console.log(myInfo);
   return (
-    <>
-      <></>
-    </>
+    <Layout>
+      <Background src="/images/practiceBackground.jpg" />
+      <ChampionBox>
+        <Champion>
+          {myInfo && (
+            <ChampionInfo>
+              <Name>{myInfo.userNick}</Name>
+              <ProfileImgBox>
+                <ImgWrapper>
+                  {myInfo.userProfilePath ? (
+                    <ProfileImg src={myInfo.userProfilePath} alt="" />
+                  ) : (
+                    <ProfileImg src="/images/defaultUser.png" alt="" />
+                  )}
+                </ImgWrapper>
+              </ProfileImgBox>
+              <Tier language={t("language")}>{t(tier[myInfo.userTier])}</Tier>
+            </ChampionInfo>
+          )}
+        </Champion>
+      </ChampionBox>
+      <center>
+        <InButton>{t("start")}</InButton>
+      </center>
+    </Layout>
   );
 }
 export default RankLobby;
--- client/src/pages/Gyeorugi/Rank/RankLobby.styled.js
+++ client/src/pages/Gyeorugi/Rank/RankLobby.styled.js
@@ -1,17 +1,100 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { colors, fontSize, fontWeight } from "_foundation";
 
-export const Layout = styled.div``;
+export const Layout = styled.div`
+  height: 100vh;
+  width: 100vw;
 
-export const Background = styled.div``;
+  /* display: flex; */
+`;
 
-export const Champion = styled.div``;
+export const Background = styled.img`
+  filter: grayscale(100%) brightness(20%);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+`;
 
-export const Name = styled.div``;
+export const ChampionBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8rem;
 
-export const ProfileImgBox = styled.div``;
+  padding-bottom: 2rem;
+  height: 85%;
 
-export const ImgWrapper = styled.div``;
+  /* background-color: blue; */
+`;
 
-export const ProfileImg = styled.img``;
+export const ChampionInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.2rem;
 
-export const Tier = styled.div``;
+  padding-top: 4rem;
+  color: ${colors.gray0};
+`;
+
+export const Champion = styled.div`
+  padding-top: 92px;
+  width: 18%;
+  height: 100%;
+  /* background-color: yellow; */
+
+  background-image: url("/images/badge.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+export const Name = styled.div`
+  font-size: ${fontSize.h4};
+  font-weight: ${fontWeight.medium};
+
+  span {
+    padding: 0 0.3rem;
+  }
+`;
+
+export const ProfileImgBox = styled.div`
+  width: 65%;
+`;
+
+export const ImgWrapper = styled.div`
+  position: relative;
+  padding-top: 100%;
+  border-radius: 100%;
+  overflow: hidden;
+`;
+
+export const ProfileImg = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  max-height: 100%;
+  width: auto;
+`;
+
+export const Tier = styled.div`
+  padding: 2rem 0;
+  ${(props) => {
+    if (props.language === "ENG") {
+      return css`
+        font-family: "Dry Brush";
+        font-size: ${fontSize.h1};
+      `;
+    } else {
+      return css`
+        font-family: "Dokdo";
+        font-size: ${fontSize.h1};
+      `;
+    }
+  }}
+`;
